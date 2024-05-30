8d4f1cc318e60476206e07db008005e26427d3a5
inyat <inyat.nathani@gmail.com>
2024-05-30 15:23:23 +0200

    Initial commit

---
+++ src/main/java/se/inyat/emailsenderservice/EmailSenderServiceApplication.java
@@ -0,0 +1,14 @@
+package se.inyat.emailsenderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmailSenderServiceApplication.class, args);
+    }
+
+}
